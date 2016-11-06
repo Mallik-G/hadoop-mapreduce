bf46c506a813e24880b55b844c0558a3122cc590
David Venturi <venturidb@gmail.com>
2016-11-06 13:57:01 -0500

    Update mapper.py

--- lesson-3-project/part-2/mapper.py
+++ lesson-3-project/part-2/mapper.py
@@ -5,7 +5,7 @@
 import sys
 
 for line in sys.stdin:
-  data = line.strip().split("/t")
-  if len(data) == 6:
+  data = line.strip().split(" ")
+  if len(data) == 10:
     host, ident, authuser, date, timezone, request_type, page, request_protocol, status, bytes = data
     print "{0}\t{1}".format(page, bytes)
