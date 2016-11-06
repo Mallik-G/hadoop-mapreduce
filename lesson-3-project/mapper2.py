5db4f3cb4f80e069e8686aecbeeff2583615ee4f
David Venturi <venturidb@gmail.com>
2016-11-06 13:46:24 -0500

    Create mapper2.py

---
+++ lesson-3-project/mapper2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Mapper code for Part 2
+
+import sys
+
+for line in sys.stdin:
+  data = line.strip().split("/t")
+  if len(data) == 6:
+    host, ident, authuser, date, timezone, request_type, page, request_protocol, status, bytes = data
+    print "{0}\t{1}".format(page, bytes)
